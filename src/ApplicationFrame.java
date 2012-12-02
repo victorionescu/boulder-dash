5af025153af9341d17eccd1b05aa2b9da0c90cb4
Victor Ionescu <victor@okapidev.com>
2012-12-03 01:23:23 +0200

    create ApplicationFrame

---
+++ src/ApplicationFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class ApplicationFrame extends JFrame {
+    public static void main(String[] args) {
+        ApplicationFrame applicationFrame = new ApplicationFrame();
+        applicationFrame.setVisible(true);
+    }
+}

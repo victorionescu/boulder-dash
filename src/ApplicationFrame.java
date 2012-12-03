4e22aa0187ce29ec3d02acfc1a6d3f12affad497
Victor Ionescu <victor@okapidev.com>
2012-12-03 03:15:07 +0200

    MacWidgets

--- src/ApplicationFrame.java
+++ src/ApplicationFrame.java
@@ -1,6 +1,27 @@
+import com.explodingpixels.macwidgets.IAppWidgetFactory;
+import com.explodingpixels.macwidgets.MacUtils;
+import com.explodingpixels.macwidgets.MacWidgetFactory;
+import com.explodingpixels.macwidgets.UnifiedToolBar;
+import com.explodingpixels.widgets.TableUtils;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.*;
 
 public class ApplicationFrame extends JFrame {
+    public ApplicationFrame() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        MacUtils.makeWindowLeopardStyle(getRootPane());
+
+        JPanel contentPane = new JPanel(new BorderLayout());
+
+        pack();
+
+        //JPanel contentPane = new JPanel(new BorderLayout());
+        //contentPane.add(toolBar, BorderLayout.NORTH);
+    }
+
     public static void main(String[] args) {
         ApplicationFrame applicationFrame = new ApplicationFrame();
         applicationFrame.setVisible(true);

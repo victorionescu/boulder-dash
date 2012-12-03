a76291e684ba87292174b195bbae119623594813
Victor Ionescu <victor@okapidev.com>
2012-12-03 20:56:31 +0200

    toolbars

--- src/ApplicationFrame.java
+++ src/ApplicationFrame.java
@@ -1,27 +1,71 @@
-import com.explodingpixels.macwidgets.BottomBar;
-import com.explodingpixels.macwidgets.BottomBarSize;
-import com.explodingpixels.macwidgets.MacUtils;
-import com.explodingpixels.macwidgets.MacWidgetFactory;
+import com.explodingpixels.macwidgets.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ApplicationFrame extends JFrame {
     public ApplicationFrame() {
         super("Boulder Dash");
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(1000, 500));
 
         MacUtils.makeWindowLeopardStyle(getRootPane());
 
         JPanel contentPane = new JPanel(new BorderLayout());
         setContentPane(contentPane);
 
+        JPanel gameScreen = new JPanel(new BorderLayout());
+        gameScreen.setBorder(BorderFactory.createLineBorder(Color.black));
+
+        contentPane.add(getBottomBar(), BorderLayout.SOUTH);
+        contentPane.add(gameScreen, BorderLayout.CENTER);
+        contentPane.add(getToolbar(), BorderLayout.NORTH);
+
+        pack();
+    }
+
+    public JComponent getToolbar() {
+        UnifiedToolBar toolBar = new UnifiedToolBar();
+
+        /*java.net.URL imgURL = getClass().getResource("blue_wall.png");
+
+        if (imgURL != null) {
+            System.out.println("YES! SCORE!");
+        } else {
+            System.out.println("NAH");
+        }
+
+        Icon imageIcon = new ImageIcon(imgURL, "ceva wall");
+
+        JComponent button = MacButtonFactory.createGradientButton(imageIcon, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                //To change body of implemented methods use File | Settings | File Templates.
+                System.out.println(getWidth() + " " + getHeight());
+            }
+        });
+
+        System.out.println(button.getWidth() + " " + button.getHeight());
+        toolBar.addComponentToCenter(button); */
+
+        /*JButton editButton = new JButton("Edit");
+        editButton.putClientProperty("JButton.buttonType", "textured");
+
+        toolBar.addComponentToLeft(editButton);  */
+
+        return toolBar.getComponent();
+
+    }
+
+    public JComponent getBottomBar() {
         BottomBar bottomBar = new BottomBar(BottomBarSize.LARGE);
         bottomBar.addComponentToCenter(MacWidgetFactory.createEmphasizedLabel("" +
                 "you and I, are like diamonds in the sky"));
 
-        contentPane.add(bottomBar.getComponent(), BorderLayout.SOUTH);
-        pack();
+        return bottomBar.getComponent();
     }
 
     public static void main(String[] args) {
---
+++ src/view/GameView.java
@@ -0,0 +1,11 @@
+package view;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: victor
+ * Date: 03/12/2012
+ * Time: 14:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameView {
+}

295b6048d07fe613f2c29b700df992f26a4fa188
Victor Ionescu <victor@okapidev.com>
2012-12-05 00:25:43 +0200

    WallElement + WallElementPainter

--- src/model/WallElement.java
+++ src/model/WallElement.java
@@ -3,7 +3,14 @@ package model;
 import java.awt.*;
 
 public class WallElement extends CaveElement {
-    public WallElement(Cave cave, Point coordinates, int color) {
+    private final Color color;
+
+    public WallElement(Cave cave, Point coordinates, Color color) {
         super(cave, coordinates);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
     }
 }
--- src/view/WallElementPainter.java
+++ src/view/WallElementPainter.java
@@ -1,8 +1,8 @@
 package view;
 
 import model.CaveElement;
+import model.WallElement;
 
-import javax.swing.*;
 import java.awt.*;
 
 public class WallElementPainter implements CaveElementPainter {
@@ -12,5 +12,12 @@ public class WallElementPainter implements CaveElementPainter {
     }
 
     public void paint(Graphics2D g, CaveElement e) {
+        WallElement wallElement = (WallElement)e;
+        g.setColor(wallElement.getColor());
+
+        int offsetX = wallElement.getX() * 30;
+        int offsetY = wallElement.getY() * 30;
+
+        g.fillRect(offsetX, offsetY, 30, 30);
     }
 }

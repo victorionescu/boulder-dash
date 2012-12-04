1a9107469cd8e4fcf22b0eb9d10e1d0154ce72f2
Victor Ionescu <victor@okapidev.com>
2012-12-04 06:11:06 +0200

    removed mac looks

--- src/ApplicationFrame.java
+++ src/ApplicationFrame.java
@@ -1,9 +1,5 @@
-import com.explodingpixels.macwidgets.*;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class ApplicationFrame extends JFrame {
     public ApplicationFrame() {
@@ -12,15 +8,12 @@ public class ApplicationFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(1000, 500));
 
-        MacUtils.makeWindowLeopardStyle(getRootPane());
-
         JPanel contentPane = new JPanel(new BorderLayout());
         setContentPane(contentPane);
 
         JPanel gameScreen = new JPanel(new BorderLayout());
         gameScreen.setBorder(BorderFactory.createLineBorder(Color.black));
 
-        contentPane.add(getBottomBar(), BorderLayout.SOUTH);
         contentPane.add(gameScreen, BorderLayout.CENTER);
         contentPane.add(getToolbar(), BorderLayout.NORTH);
 
@@ -28,44 +21,9 @@ public class ApplicationFrame extends JFrame {
     }
 
     public JComponent getToolbar() {
-        UnifiedToolBar toolBar = new UnifiedToolBar();
+        JToolBar toolBar = new JToolBar();
 
-        /*java.net.URL imgURL = getClass().getResource("blue_wall.png");
-
-        if (imgURL != null) {
-            System.out.println("YES! SCORE!");
-        } else {
-            System.out.println("NAH");
-        }
-
-        Icon imageIcon = new ImageIcon(imgURL, "ceva wall");
-
-        JComponent button = MacButtonFactory.createGradientButton(imageIcon, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                //To change body of implemented methods use File | Settings | File Templates.
-                System.out.println(getWidth() + " " + getHeight());
-            }
-        });
-
-        System.out.println(button.getWidth() + " " + button.getHeight());
-        toolBar.addComponentToCenter(button); */
-
-        /*JButton editButton = new JButton("Edit");
-        editButton.putClientProperty("JButton.buttonType", "textured");
-
-        toolBar.addComponentToLeft(editButton);  */
-
-        return toolBar.getComponent();
-
-    }
-
-    public JComponent getBottomBar() {
-        BottomBar bottomBar = new BottomBar(BottomBarSize.LARGE);
-        bottomBar.addComponentToCenter(MacWidgetFactory.createEmphasizedLabel("" +
-                "you and I, are like diamonds in the sky"));
-
-        return bottomBar.getComponent();
+        return toolBar;
     }
 
     public static void main(String[] args) {

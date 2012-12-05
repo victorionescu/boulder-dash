ab733702ca50cebc158bd15095e86c362dc8d6fd
Victor Ionescu <victor@okapidev.com>
2012-12-05 11:21:27 +0200

    added resources/ folder with icons

--- src/model/Cave.java
+++ src/model/Cave.java
@@ -4,9 +4,11 @@ import java.awt.*;
 
 public class Cave {
     private Dimension dimension;
+    private CaveElement[][] elements;
 
     public Cave(Dimension dimension) {
         this.dimension = dimension;
+        elements = new CaveElement[dimension.width][dimension.height];
     }
 
     public int getWidth() {
